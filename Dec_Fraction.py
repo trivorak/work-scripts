db0344726fc188ba30a5ff6b95f49de30365d1e7
Drake Champagne <trivorak@gmail.com>
2017-07-31 11:44:18 -0500

    New script for Decimal to Nearest 64th Radius Gage

--- Dec_Fraction.py
+++ Dec_Fraction.py
@@ -1,6 +1,5 @@
 from fractions import Fraction
-s = float(input('MM Radius? '))
-s = s/25.4
+s = float(input('Decimal Radius? '))
 s = Fraction(round(s/(1/64),0)*(1/64))
 print(s)
 l = input('Press Enter to close this script')
