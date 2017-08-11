1cf459ef18badc1bf7331f23ef053ab94d08f68b
Drake Champagne <trivorak@gmail.com>
2017-08-11 16:15:14 -0500

    Added a New calc for work. Bad code but it works, Get fucked.

---
+++ Fiber_block.py
@@ -0,0 +1,31 @@
+s = float(input('Thickness of part = '))
+t = float(input('Lens Distance = '))
+u = float(input('Fiber Distance = '))
+v = abs(t-(s-u))
+
+if v>.157:
+    a = round(v-.157,4)
+    v = str(round(v,4))
+    a = str(a)
+    print(v + ' Actual')
+    print(a + ' OMT')
+    print()
+    print('Part is Bad')
+    print()
+elif v<.147:
+    a = round(.147-v,v)
+    v = str(round(v,4))
+    a = str(a)
+    print(v + ' Actual')
+    print(a + ' UMT')
+    print()
+    print('Part is Bad')
+    print()
+else:
+    v= str(round(v,4))
+    print(v + ' Actual')
+    print()
+    print('Part is Good')
+    print()
+    
+x = input('Press Enter to Exit, confusing I know')
