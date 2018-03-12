e7fc0b08346f6b3d3524e488f8bf0920225b701a
trivorak <trivorak@gmail.com>
2018-03-12 09:59:28 -0500

    Added CMM Merge File Batch

---
+++ merge_get.bat
@@ -0,0 +1,3 @@
+copy "C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__chr.txt" "char.txt"
+copy "C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__fet.txt" "feat.txt"
+copy "C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__hdr.txt" "hdr.txt"
