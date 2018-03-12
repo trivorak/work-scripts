ab3d52143132757fd0f6a8026fe884a74cbf613c
trivorak <trivorak@gmail.com>
2018-03-12 12:28:08 -0500

    Added 2 new files init_merge and merge_copy_with_date to the repo

---
+++ init_merge.bat
@@ -0,0 +1,4 @@
+@echo no
+type NUL >"C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__chr.txt"
+type NUL >"C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__fet.txt"
+type NUL >"C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__hdr.txt"
---
+++ merge_copy_with_date.bat
@@ -0,0 +1,7 @@
+@echo off
+copy "C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__chr.txt" "merge_chr- %date:/=-% .txt"
+copy "C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__fet.txt" "merge_fet- %date:/=-% .txt"
+copy "C:\Program Files\Zeiss\Calypso\home\om\workarea\results\merge__hdr.txt" "merge_hdr- %date:/=-% .txt"
+
+
+
